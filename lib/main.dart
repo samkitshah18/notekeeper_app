e90cfbf7bc7150849db3eed7eb73ad2affe836f4
samkitshah18 <samkitvshah18@gmail.com>
2020-08-09 22:05:14 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:notekeeperapp/screens/note_list.dart';
+import 'package:notekeeperapp/screens/note_detail.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      title: 'NoteKeeper',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          primarySwatch: Colors.deepPurple
+      ),
+      home: NoteList(),
+    );
+  }
+}
+
